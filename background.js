46737ede1cc634b03971e2974766b74852a34616
Matt Baker <matt@gladly.com>
2022-07-08 09:23:50 -0700

    Improvements

--- background.js
+++ background.js
@@ -1,47 +1,74 @@
-chrome.tabs.onUpdated.addListener((tabId, tab) => {
+/*
+ * background.js runs in the background on Chrome. It has access to manage the windows/tabs.
+ * This will start the process to redirect the open tab into the PWA.
+ */
 
-  if (tab.url && tab.url.includes("meet.google.com")) {
-    const queryParameters = tab.url.split("/")[3].split("?")[0];
-    const urlParameters = new URLSearchParams(queryParameters);
+let googleMeetWindowId;
 
-	  if (queryParameters != 'new' && queryParameters != '_meet'){
-    console.log('URL matches');
-    console.log(queryParameters);
-    console.log(tab.url);
-    chrome.storage.local.set({"tabId": tabId}, function(){
-	console.log('saved tabId to storage:' + tabId);
-    });
+chrome.tabs.onUpdated.addListener((tabId, tabChangeInfo, tab) => {
+  // when tab is loaded on meet.google.com, attempt to redirect
+  if (
+    tabChangeInfo.status === 'complete' &&
+    tab.url &&
+    tab.url.includes('meet.google.com')
+  ) {
+    // find Google Meet PWA window id
+    chrome.windows.getAll(
+      { populate: true, windowTypes: ['app'] },
+      function (windows) {
+        windows.forEach((window) => {
+          if (
+            window.tabs.length === 1 &&
+            window.tabs[0].url.startsWith('https://meet.google.com/')
+          ) {
+            googleMeetWindowId = window.id;
+          }
+        });
 
+        if (!googleMeetWindowId) {
+          // skipping redirect since PWA isn't open
+          // we could inject a button onto the page to inform the user of this
+          return;
+        }
 
-    chrome.storage.local.set({"newTab": queryParameters}, function(){
-      console.log('saved to storage:' + queryParameters);
-    });
-
-
-	  }
-  }
-});
-
-
-chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-    console.log('CHANGE DETECTED');
-    console.log(newValue);
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
+        // only attempt a redirect when not the PWA
+        if (tab.windowId !== googleMeetWindowId) {
+          chrome.storage.local.set(
+            { originatingTabId: '', queryParams: '' },
+            function () {
+              let queryParameters = tab.url.split('/')[3].split('?')[0];
+              if (queryParameters !== 'new' && queryParameters !== '_meet') {
+                // if empty, set the the landing page
+                queryParameters = queryParameters ? queryParameters : 'landing';
+                console.log(`queryParameters: ${queryParameters}`);
+                chrome.storage.local.set({
+                  originatingTabId: tabId,
+                  queryParams: queryParameters,
+                });
+              }
+            }
+          );
+        }
+      }
     );
-    if (key == 'opened'){
-      //attempt to close tab
-      console.log('attempt to close tab now');
-      chrome.storage.local.get(['tabId'], function(result) {
-        console.log('tabId currently is ' + result.tabId);
-        console.log(result);
-        		   console.log('attempting to close original tab');
-        		   setTimeout(function(){chrome.tabs.remove(result.tabId);},3000);
-      });
-    }
   }
 });
 
-
+chrome.storage.onChanged.addListener(function (changes) {
+  if (changes['googleMeetOpenedUrl']) {
+    // bring Google Meet PWA into focus
+    chrome.windows.update(googleMeetWindowId, { focused: true }, function () {
+      // close the tab that originally started the process if it wasn't the landing page
+      chrome.storage.local.get(
+        ['originatingTabId', 'queryParams'],
+        function (result) {
+          setTimeout(function () {
+            if (result.queryParams !== 'landing') {
+              chrome.tabs.remove(result.originatingTabId);
+            }
+          }, 3000);
+        }
+      );
+    });
+  }
+});
--- contentScript.js
+++ contentScript.js
@@ -1,88 +1,59 @@
+/*
+ * contentScript.js is injected onto any meet.google.com page. This has different logic depending on if
+ * it is running in the PWA or a normal tab. The PWA portion will redirect it to the correct meeting
+ * (if not currently on a meeting). The normal tab will replace the content on the original page
+ * informing the user they were redirected to the PWA.
+ */
+
 (() => {
-    console.log('Detected Google Meet');
-    console.log(window.document.title);
+  if (isPwa()) {
+    chrome.storage.onChanged.addListener(function (changes) {
+      if (changes['queryParams'] && changes['queryParams'].newValue) {
+        const icons = document.getElementsByClassName('google-material-icons');
+        let onCall = false;
+        for (const i in icons) {
+          if (icons[i].outerText === 'call_end') {
+            onCall = true;
+          }
+        }
 
+        if (onCall) {
+          return;
+        }
 
-    const standalone = window.matchMedia('(display-mode: standalone)').matches;
-    const fullscreen = window.matchMedia('(display-mode: fullscreen)').matches;
-    const minimal = window.matchMedia('(display-mode: minimal-ui)').matches;
-    
-if (standalone) {
-  // Don't run
-  console.log('standalone');
-} else {
-  // Run
-}
+        window.location.href =
+          'https://meet.google.com/' +
+          changes['queryParams'].newValue +
+          '?authuser=0';
 
-if (fullscreen) {
-  console.log('fullscreen');
-  // Don't run
-} else {
-  // Run
-}
-
-if (minimal) {
-  console.log('minimal');
-	//detected PWA
-	//save tab id to Storage
-	chrome.storage.local.set({"pwaId": 123}, function(){
-	console.log('saved pwaId to storage');
-	chrome.storage.local.get(['newTab'], function(result) {
-  		console.log('newTab value currently is ' + result.newTab);
-		console.log('monitoring for updates now');
-
-		chrome.storage.onChanged.addListener(function (changes, namespace) {
-  		for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
- 
-	 		 console.log(oldValue);
-    			console.log(
-      			`Storage key "${key}" in namespace "${namespace}" changed.`,
-      			`Old value was "${oldValue}", new value is "${newValue}".`
-    			);
-	  		if (newValue != 'new' && newValue != '_meet') {
-		  		console.log('opening new location:');
-		  		console.log(newValue);
-
-		  		setTimeout(function(){ 
-					window.location.href = "https://meet.google.com/"+newValue+"?authuser=0";
-	  				//close original tab
-					chrome.storage.local.set({"opened": newValue}, function(){
-						console.log('saved OPENED to storage');
-					});
-				},500);
-	  		};
-
-  		}
-		});
-	});
-    	});
-} else {
-  // Run
-	// listen for opened events
-	//
-	chrome.storage.onChanged.addListener(function (changes, namespace) {
-  for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
- 
-	  console.log('OPEN DETECTED');
-	  console.log(newValue);
-	  chrome.tabs
-    console.log(
-      `Storage key "${key}" in namespace "${namespace}" changed.`,
-      `Old value was "${oldValue}", new value is "${newValue}".`
-    );
-	   chrome.storage.local.get(['tabId'], function(result) {
-  console.log('tabId currently is ' + result.tabId);
-  console.log(result);
-    if (key == 'opened'){		   
-		   console.log('attempting to close original tab');
-		   console.log(document.body.childNodes[1].style.display="none");
-		   const textnode = document.createTextNode("Opening in Google Meet app");
-		   document.body.appendChild(textnode);
-		   chrome.tabs.remove(result.tabId);
-    }
-});
-  }});
-}
-
+        // close original tab
+        chrome.storage.local.set(
+          {
+            googleMeetOpenedUrl: new Date().toISOString(),
+          },
+          function () {
+            console.log('saved OPENED to storage');
+          }
+        );
+      }
+    });
+  } else {
+    // Normal tab, add listener to replace UI with
+    chrome.storage.onChanged.addListener(function (changes) {
+      if (changes['originatingTabId'] && changes['originatingTabId'].newValue) {
+        // could improve this. it only properly replaces if you navigated to a meet.google.com/some-slug
+        // it does not know how to replace the landing page. we could make it look a lot nicer
+        document.body.childNodes[1].style.display = 'none';
+        const textnode = document.createTextNode('Opening in Google Meet app');
+        document.body.appendChild(textnode);
+      }
+    });
+  }
 })();
 
+function isPwa() {
+  return ['fullscreen', 'standalone', 'minimal-ui'].some(
+    (displayMode) =>
+      window.matchMedia('(display-mode: ' + displayMode + ')').matches
+  );
+}
