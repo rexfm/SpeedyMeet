cda823f665e4b19e072f487a106c9b9d93b7782c
Matt Baker <matt@gladly.com>
2022-07-13 14:15:55 -0700

    Add options

--- contentScript.js
+++ contentScript.js
@@ -7,20 +7,86 @@
 
 (() => {
   if (isPwa()) {
+    let settings = {};
+    const settingsToMonitor = [
+      'stopVideoOnJoin',
+      'stopMicOnJoin',
+      'spaceBarToUnmute',
+      'skipPreMeeting',
+    ];
+    chrome.storage.sync.get(settingsToMonitor, (values) => {
+      settings = values;
+    });
+    chrome.storage.onChanged.addListener(function (changes, area) {
+      if (area === 'sync') {
+        settingsToMonitor.forEach((setting) => {
+          if (changes[setting]) {
+            settings[setting] = changes[setting].newValue;
+          }
+        });
+      }
+    });
+
+    window.onbeforeunload = function (e) {
+      if (isOnCall()) {
+        e.stopPropagation();
+        var message = 'Do you want to leave this page?';
+        return message;
+      }
+    };
+
+    window.onload = function (e) {
+      console.log('window.onload');
+
+      const spans = document.querySelectorAll('span');
+      for (const s in spans) {
+        if (spans[s].outerText === 'Join now') {
+          if (settings.stopVideoOnJoin) {
+            toggleVideo();
+          }
+
+          if (settings.stopMicOnJoin) {
+            toggleMute();
+          }
+
+          if (settings.skipPreMeeting) {
+            setTimeout(() => {
+              spans[s].click();
+            }, 500);
+            console.log('click join now');
+          }
+        }
+      }
+    };
+
+    document.addEventListener('keydown', function (e) {
+      if (settings.spaceBarToUnmute && e.code === 'Space') {
+        if (e.target && ['BODY', 'BUTTON'].includes(e.target.nodeName)) {
+          e.preventDefault();
+          if (!!getTooltipByName('Turn on microphone (⌘ + d)')) {
+            toggleMute();
+          }
+        }
+      }
+    });
+
+    document.addEventListener('keyup', function (e) {
+      if (settings.spaceBarToUnmute && e.code === 'Space') {
+        if (e.target && ['BODY', 'BUTTON'].includes(e.target.nodeName)) {
+          e.preventDefault();
+          if (!!getTooltipByName('Turn off microphone (⌘ + d)')) {
+            toggleMute();
+          }
+        }
+      }
+    });
+
     chrome.storage.onChanged.addListener(function (changes) {
       if (
         changes['queryParams'] &&
         changes['queryParams'].newValue !== '__gmInitialState'
       ) {
-        const icons = document.getElementsByClassName('google-material-icons');
-        let onCall = false;
-        for (const i in icons) {
-          if (icons[i].outerText === 'call_end') {
-            onCall = true;
-          }
-        }
-
-        if (onCall) {
+        if (isOnCall()) {
           return;
         }
 
@@ -59,5 +125,46 @@ function isPwa() {
   return ['fullscreen', 'standalone', 'minimal-ui'].some(
     (displayMode) =>
       window.matchMedia('(display-mode: ' + displayMode + ')').matches
+  );
+}
+
+function isOnCall() {
+  const icons = document.getElementsByClassName('google-material-icons');
+  for (const i in icons) {
+    if (icons[i].outerText === 'call_end') {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+function getTooltipByName(name) {
+  const tooltips = document.querySelectorAll('[role="tooltip"]');
+  for (const t in tooltips) {
+    if (tooltips[t].outerText === name) {
+      return tooltips[t];
+    }
+  }
+
+  return undefined;
+}
+
+function toggleMute() {
+  console.log('toggle function');
+  document.dispatchEvent(
+    new KeyboardEvent('keydown', {
+      key: 'd',
+      metaKey: true, // these are here for example's sake.
+    })
+  );
+}
+
+function toggleVideo() {
+  document.dispatchEvent(
+    new KeyboardEvent('keydown', {
+      key: 'e',
+      metaKey: true, // these are here for example's sake.
+    })
   );
 }
--- popup.js
+++ popup.js
@@ -1,5 +1,36 @@
-window.addEventListener('click',function(e){
-  if(e.target.href!==undefined){
-    chrome.tabs.create({url:e.target.href})
-  }
-})
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var setValues = function (values) {
+    $('#stopVideoOnJoin').prop('checked', values.stopVideoOnJoin);
+    $('#stopMicOnJoin').prop('checked', values.stopMicOnJoin);
+    $('#spaceBarToUnmute').prop('checked', values.spaceBarToUnmute);
+    $('#skipPreMeeting').prop('checked', values.skipPreMeeting);
+  };
+  chrome.storage.sync.get(
+    ['stopVideoOnJoin', 'stopMicOnJoin', 'spaceBarToUnmute', 'skipPreMeeting'],
+    function (values) {
+      console.log(values);
+      setValues(values);
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('loaded?');
+  restore_options();
+
+  $('.form-check-input').on('click', function () {
+    console.log('called?');
+    const enabled = $(this).is(':checked');
+    const key = $(this).attr('id');
+    console.log(`key = ${key} || enabled = ${enabled}`);
+    chrome.storage.sync.set(
+      {
+        [key]: enabled,
+      },
+      function () {
+        console.log('Settings saved');
+      }
+    );
+  });
+});
