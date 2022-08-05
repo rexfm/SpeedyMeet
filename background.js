d5fd14ec4a58b818e5350c13e6151f4c7c61d19f
Matt Baker <matt@gladly.com>
2022-08-04 22:27:00 -0700

    remove special handling for new

--- background.js
+++ background.js
@@ -11,80 +11,59 @@ chrome.tabs.onCreated.addListener(() => {
   chrome.storage.local.set({
     originatingTabId: '',
     queryParams: '__gmInitialState',
-    source: '',
   });
 });
 
-function getGoogleMeetWindow() {
-  if (!googleMeetWindowId) {
-    chrome.windows.getAll(
-      { populate: true, windowTypes: ['app'] },
-      function (windows) {
-        windows.forEach((window) => {
-          if (
-            window.tabs.length === 1 &&
-            window.tabs[0].url.startsWith('https://meet.google.com/')
-          ) {
-            googleMeetWindowId = window.id;
-          }
-        });
-      }
-    );
-  }
-}
-
-chrome.tabs.onUpdated.addListener((tabId, tabChangeInfo, tab) => {
-  getGoogleMeetWindow();
-  if (!googleMeetWindowId) {
+function getGoogleMeetWindow(runIfFound = () => {}) {
+  if (googleMeetWindowId) {
+    runIfFound();
     return;
   }
 
-  // Special handling if it's a "/new" URL
-  // This allows users to send follow-up slack from the PWA
-  if (tab.url && tab.url.includes('meet.google.com/new')) {
-    // only attempt a redirect when not the PWA
-    if (tab.windowId !== googleMeetWindowId) {
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tab.id },
-          injectImmediately: true,
-          func: () => {
-            window.stop();
-          },
-        },
-        function () {
-          const queryParameters = tab.url.split('/')[3];
-          chrome.storage.local.set({
-            originatingTabId: tabId,
-            queryParams: queryParameters,
-            source: 'NEW_MEETING',
-          });
+  chrome.windows.getAll(
+    { populate: true, windowTypes: ['app'] },
+    function (windows) {
+      windows.forEach((window) => {
+        if (
+          window.tabs.length === 1 &&
+          window.tabs[0].url.startsWith('https://meet.google.com/')
+        ) {
+          googleMeetWindowId = window.id;
         }
-      );
+      });
+
+      if (googleMeetWindowId) {
+        runIfFound();
+      }
     }
-  } else if (tab.url && tab.url.includes('meet.google.com')) {
-    // find Google Meet PWA window id
-    // only attempt a redirect when not the PWA
-    if (tab.windowId !== googleMeetWindowId) {
-      const queryParameters = tab.url.split('/')[3].split('?')[0];
-      chrome.scripting.executeScript(
-        {
-          target: { tabId: tab.id },
-          injectImmediately: true,
-          func: () => {
-            window.stop();
+  );
+}
+
+chrome.tabs.onUpdated.addListener((tabId, tabChangeInfo, tab) => {
+  getGoogleMeetWindow(() => {
+    if (tab.url && tab.url.includes('meet.google.com')) {
+      // find Google Meet PWA window id
+      // only attempt a redirect when not the PWA
+      if (tab.windowId !== googleMeetWindowId) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tab.id },
+            injectImmediately: true,
+            func: () => {
+              window.stop();
+            },
           },
-        },
-        function () {
-          const queryParameters = tab.url.split('/')[3];
-          chrome.storage.local.set({
-            originatingTabId: tabId,
-            queryParams: queryParameters,
-          });
-        }
-      );
+          function () {
+            const queryParameters = tab.url.split('/')[3];
+            chrome.storage.local.set({
+              originatingTabId: tabId,
+              queryParams: queryParameters,
+            });
+          }
+        );
+      }
     }
-  }
+  });
 });
 
 chrome.storage.onChanged.addListener(function (changes) {
@@ -93,8 +72,8 @@ chrome.storage.onChanged.addListener(function (changes) {
     chrome.windows.update(googleMeetWindowId, { focused: true }, function () {
       // close the tab that originally started the process if it wasn't the landing page
       chrome.storage.local.get(
-        ['originatingTabId', 'queryParams', 'source'],
-        function ({ originatingTabId, queryParams, source }) {
+        ['originatingTabId', 'queryParams'],
+        function ({ originatingTabId, queryParams }) {
           setTimeout(function () {
             if (queryParams !== '') {
               chrome.tabs.remove(originatingTabId);
