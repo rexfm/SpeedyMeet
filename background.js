c26a11dde106354b1e0f6c515064314068d1e392
Matt Baker <mrbaker4@users.noreply.github.com>
2022-08-28 13:16:26 -0700

    Add additional functionality to SpeedyMeet (#6)
    * Improvements
    
    * Add ability to handle '/new' google meet urls
    
    * Add options
    
    * Refactor and improve transitions
    
    * remove special handling for new
    
    * Always close quickly
    
    Co-authored-by: Rex Pechler <rex@gladly.com>

--- background.js
+++ background.js
@@ -3,99 +3,68 @@
  * This will start the process to redirect the open tab into the PWA.
  */
 
+
 let googleMeetWindowId;
 
 // clear referring state on page load
 chrome.tabs.onCreated.addListener(() => {
+  getGoogleMeetWindow();
   chrome.storage.local.set({
     originatingTabId: '',
     queryParams: '__gmInitialState',
-    source: '',
   });
 });
 
-chrome.tabs.onUpdated.addListener((tabId, tabChangeInfo, tab) => {
-  if (tab.url && tab.url.includes('meet.google.com/new')) {
-    // Special handling if it's a "/new" URL
-    // This allows users to send follow-up slack from the PWA
-    chrome.windows.getAll(
-      { populate: true, windowTypes: ['app'] },
-      function (windows) {
-        windows.forEach((window) => {
-          if (
-            window.tabs.length === 1 &&
-            window.tabs[0].url.startsWith('https://meet.google.com/')
-          ) {
-            googleMeetWindowId = window.id;
-          }
-        });
+function getGoogleMeetWindow(runIfFound = () => {}) {
+  if (googleMeetWindowId) {
+    runIfFound();
+    return;
+  }
 
-        if (!googleMeetWindowId) {
-          // skipping redirect since PWA isn't open
-          // we could inject a button onto the page to inform the user of this
-          return;
+  chrome.windows.getAll(
+    { populate: true, windowTypes: ['app'] },
+    function (windows) {
+      windows.forEach((window) => {
+        if (
+          window.tabs.length === 1 &&
+          window.tabs[0].url.startsWith('https://meet.google.com/')
+        ) {
+          googleMeetWindowId = window.id;
         }
+      });
 
-        // only attempt a redirect when not the PWA
-        if (tab.windowId !== googleMeetWindowId) {
-          chrome.scripting.executeScript(
-            {
-              target: { tabId: tab.id },
-              injectImmediately: true,
-              func: () => {
-                window.stop();
-              },
-            },
-            function () {
-              const queryParameters = tab.url.split('/')[3];
-              chrome.storage.local.set({
-                originatingTabId: tabId,
-                queryParams: queryParameters,
-                source: 'NEW_MEETING',
-              });
-            }
-          );
-        }
+      if (googleMeetWindowId) {
+        runIfFound();
       }
-    );
-  } else if (
-    tabChangeInfo.status === 'complete' &&
-    tab.url &&
-    tab.url.includes('meet.google.com')
-  ) {
-    // find Google Meet PWA window id
-    chrome.windows.getAll(
-      { populate: true, windowTypes: ['app'] },
-      function (windows) {
-        windows.forEach((window) => {
-          if (
-            window.tabs.length === 1 &&
-            window.tabs[0].url.startsWith('https://meet.google.com/')
-          ) {
-            googleMeetWindowId = window.id;
-          }
-        });
+    }
+  );
+}
 
-        if (!googleMeetWindowId) {
-          // skipping redirect since PWA isn't open
-          // we could inject a button onto the page to inform the user of this
-          return;
-        }
-
-        // only attempt a redirect when not the PWA
-        if (tab.windowId !== googleMeetWindowId) {
-          const queryParameters = tab.url.split('/')[3].split('?')[0];
-          if (queryParameters !== 'new' && queryParameters !== '_meet') {
-            // if empty, set the the landing page
+chrome.tabs.onUpdated.addListener((tabId, tabChangeInfo, tab) => {
+  getGoogleMeetWindow(() => {
+    if (tab.url && tab.url.includes('meet.google.com')) {
+      // find Google Meet PWA window id
+      // only attempt a redirect when not the PWA
+      if (tab.windowId !== googleMeetWindowId) {
+        chrome.scripting.executeScript(
+          {
+            target: { tabId: tab.id },
+            injectImmediately: true,
+            func: () => {
+              window.stop();
+            },
+          },
+          function () {
+            const queryParameters = tab.url.split('/')[3];
             chrome.storage.local.set({
               originatingTabId: tabId,
               queryParams: queryParameters,
             });
           }
-        }
+        );
       }
-    );
-  }
+    }
+  });
 });
 
 chrome.storage.onChanged.addListener(function (changes) {
@@ -104,19 +73,14 @@ chrome.storage.onChanged.addListener(function (changes) {
     chrome.windows.update(googleMeetWindowId, { focused: true }, function () {
       // close the tab that originally started the process if it wasn't the landing page
       chrome.storage.local.get(
-        ['originatingTabId', 'queryParams', 'source'],
-        function ({ originatingTabId, queryParams, source }) {
-          let timeout = 3000;
-          if (source === 'NEW_MEETING') {
-            timeout = 0;
-          }
+        ['originatingTabId'],
+        function ({ originatingTabId }) {
           setTimeout(function () {
-            if (queryParams !== '') {
-              chrome.tabs.remove(originatingTabId);
-            }
-          }, timeout);
+            chrome.tabs.remove(originatingTabId);
+          }, 0);
         }
       );
     });
   }
 });
+
--- popup.js
+++ popup.js
@@ -1,5 +1,41 @@
-window.addEventListener('click',function(e){
-  if(e.target.href!==undefined){
-    chrome.tabs.create({url:e.target.href})
-  }
-})
+// Restores select box state to saved value from localStorage.
+function restore_options() {
+  var setValues = function (values) {
+    $('#stopVideoOnJoin').prop('checked', values.stopVideoOnJoin);
+    $('#stopMicOnJoin').prop('checked', values.stopMicOnJoin);
+    $('#spaceBarToUnmute').prop('checked', values.spaceBarToUnmute);
+    $('#skipPreMeeting').prop('checked', values.skipPreMeeting);
+    $('#skipPostMeeting').prop('checked', values.skipPostMeeting);
+  };
+  chrome.storage.sync.get(
+    [
+      'stopVideoOnJoin',
+      'stopMicOnJoin',
+      'spaceBarToUnmute',
+      'skipPreMeeting',
+      'skipPostMeeting',
+    ],
+    function (values) {
+      setValues(values);
+    }
+  );
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('loaded?');
+  restore_options();
+
+  $('.form-check-input').on('click', function () {
+    console.log('called?');
+    const enabled = $(this).is(':checked');
+    const key = $(this).attr('id');
+    chrome.storage.sync.set(
+      {
+        [key]: enabled,
+      },
+      function () {
+        console.log('Settings saved');
+      }
+    );
+  });
+});
